1cea13946450a4f2e9bd5ebe78d454f5bbee43a0
tamborine996 <mqc200@gmail.com>
2024-11-21 13:32:17 +0000

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
