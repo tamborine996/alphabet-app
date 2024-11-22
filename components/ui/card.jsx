76e4cc2d52fed98102a299d980b477db2d7c9105
tamborine996 <mqc200@gmail.com>
2024-11-22 11:17:46 +0000

    Update card.jsx

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -3,14 +3,14 @@ import * as React from "react";
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className="rounded-lg border bg-white text-gray-950 shadow-sm"
+    className="rounded-lg border bg-white text-gray-950 shadow-sm transition-all hover:shadow-md"
     {...props}
   />
 ));
 Card.displayName = "Card";
 
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className="p-6" {...props} />
+  <div ref={ref} className="p-4 sm:p-6" {...props} />
 ));
 CardContent.displayName = "CardContent";
 
